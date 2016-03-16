4e1479335e8d90f7cee6be4795268d369294a7c8
Georgy Kagan <georgy.gisha@gmail.com>
2016-03-16 15:11:29 +0200

    add getCampaign() & getMessageStatistics()

--- src/Timenomad/GetResponseV3Api.php
+++ src/Timenomad/GetResponseV3Api.php
@@ -3,8 +3,8 @@
 namespace Timenomad;
 
 class GetResponseV3Api {
-    const CONSENT_URL      = 'https://app.getresponse.com/oauth2_authorize.html?response_type=code&client_id={{clientId}}&state={{state}}';
-    const ACCESS_TOKEN_URL = 'https://api.getresponse.com/v3/token';
+    const CONSENT_URL = 'https://app.getresponse.com/oauth2_authorize.html?response_type=code&client_id={{clientId}}&state={{state}}';
+    const API_URL     = 'https://api.getresponse.com/v3';
 
     protected $clientId;
     protected $clientSecret;
@@ -34,7 +34,7 @@ class GetResponseV3Api {
         if (empty($_GET['state']) || $_GET['state'] !== $this->state) {
             trigger_error('No "state" GET parameter or returned value doesn\'t match the stored one.', E_USER_ERROR);
         }
-        $response = $this->doHttpRequest(self::ACCESS_TOKEN_URL, true, [
+        $response = $this->doHttpRequest(self::API_URL . '/token', true, [
             "grant_type" => "authorization_code",
             "code"       => $_GET['code']
         ]);
@@ -43,6 +43,9 @@ class GetResponseV3Api {
     }
 
     public function setAccessToken($accessToken) {
+        if (empty($accessToken)) {
+            trigger_error('No Access Token.', E_USER_ERROR);
+        }
         $this->accessToken = $accessToken;
     }
 
@@ -51,10 +54,43 @@ class GetResponseV3Api {
     //
 
     public function getAccountInfo() {
-        if (empty($this->accessToken)) {
-            trigger_error('No Access Token.', E_USER_ERROR);
+        $response = $this->doHttpRequest(self::API_URL . '/accounts');
+
+        return $response;
+    }
+
+    public function getCampaigns() {
+        $response = $this->doHttpRequest(self::API_URL . '/campaigns');
+
+        return $response;
+    }
+
+    /**
+     * @param string $campaignId
+     * @param string $metric One of: list-size, locations, origins, removals, subscriptions, balance, summary
+     * @param string $groupBy One of: hour, day, month, total
+     * @param string $fromTime Date or unix timestamp
+     * @param string $toTime Date or unix timestamp
+     * @param string $fields
+     * @return mixed
+     */
+    public function getMessageStatistics($campaignId, $metric, $groupBy = 'day', $fromTime = null, $toTime = null, $fields = null) {
+        if (empty($campaignId)) {
+            trigger_error('Campaign ID is missing.', E_USER_ERROR);
         }
-        $response = $this->doHttpRequest('https://api.getresponse.com/v3/accounts');
+        $payload = [
+            'query[campaignId]' => $campaignId,
+            'query[groupBy]' => $groupBy,
+        ];
+        if ($fromTime && $toTime) {
+            $payload['query[createdOn][from]'] = strftime('%Y-%m-%d', strtotime($fromTime) ? strtotime($fromTime) : $fromTime);
+            $payload['query[createdOn][to]'] = strftime('%Y-%m-%d', strtotime($toTime) ? strtotime($toTime) : $toTime);
+        }
+
+        if (!empty($fields)) {
+            $payload['fields'] = $fields;
+        }
+        $response = $this->doHttpRequest(self::API_URL . '/campaigns/statistics/' . $metric, false, $payload);
 
         return $response;
     }
@@ -69,6 +105,11 @@ class GetResponseV3Api {
         } else {
             $authHeader = "Authorization: Basic " . base64_encode("{$this->clientId}:{$this->clientSecret}");
         }
+
+        // if GET and got payload - send as query params
+        if (!$isPost && !empty($payload)) {
+            $url .= '?' . http_build_query($payload);
+        }
         $ch = curl_init($url);
 
         curl_setopt_array($ch, [
@@ -79,7 +120,9 @@ class GetResponseV3Api {
             CURLOPT_POST           => $isPost,
             CURLOPT_RETURNTRANSFER => true
         ]);
-        if (!empty($payload)) {
+
+        // If POST and got payload - send as POST data
+        if ($isPost && !empty($payload)) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
         }
         $response = json_decode(curl_exec($ch), true);
