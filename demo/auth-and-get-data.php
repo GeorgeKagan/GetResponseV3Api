286393520726d3cc4af312a7a33b11fbf5f23d6d
Georgy Kagan <georgy.gisha@gmail.com>
2016-03-17 14:53:57 +0200

    add demo.php

---
+++ demo/auth-and-get-data.php
@@ -0,0 +1,54 @@
+<?php
+
+require('..\\src\\Timenomad\\Http.php');
+require('..\\src\\Timenomad\\OAuth2.php');
+require('..\\src\\Timenomad\\GetResponseV3Api.php');
+
+function dd($var) {
+    echo '<pre>';
+    var_dump($var);
+    exit;
+}
+
+// Init API object
+$getResponse = new \Timenomad\GetResponseV3Api(
+    'your client id',
+    'your client secret',
+    'SDgh43r098udfsdF#$%2' // or any other combination of characters
+);
+
+// Save token to a file (will probably be a database in real world use)
+$token = file_get_contents('token.txt');
+
+// OAuth2 flow
+if (empty($token)) {
+    // User approved, Redirect Url is called from GetResponse's servers
+    if (isset($_GET['code'])) {
+        // Exchange code for an Access Token (along with Refresh Token and more)
+        $accessToken = $getResponse->getAccessToken();
+        file_put_contents('token.txt', json_encode($accessToken));
+        header('Location: /');
+    } else {
+        header('Location: ' . $getResponse->getOAuthConsentUrl());
+    }
+    exit;
+}
+
+// Use API to get data
+$token = json_decode($token, true);
+
+// Important to set the Access Token before issuing any API calls
+$getResponse->setAccessToken($token['access_token']);
+$getResponse->setAccessTokenExpiryCallback($token['refresh_token'], function($newTokenData) {
+    // Access Token expired. This function will be called after a new token has been generated.
+    // Here you can save the new token data to the database, or any other storage means.
+    file_put_contents('token.txt', json_encode($newTokenData));
+});
+
+// API methods:
+$userInfo = $getResponse->getAccountInfo();
+$campaigns = $getResponse->getCampaigns();
+$statistics = $getResponse->getCampaignStatistics($campaigns[0]['campaignId'], 'list-size');
+
+// Echo away...
+dd($statistics);
